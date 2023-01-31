e7fb7f99969373124a0c7a8903bec39499371265
JaparmySholly <japarmysholly@gmail.com>
2023-01-31 22:32:11 +0000

    DOne

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Ruby script that accepts one argument and pass it to a regular expression matching method. Regexp must match `School`.
+# A regular expression that is simply matching School
 puts ARGV[0].scan(/School/).join
